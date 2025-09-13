1c9228b686358d8518955b1d6df32904757569b9
Lkthuman <romlebgg5@gmail.com>
2025-09-13 15:42:33 +0200

    Update vald_scraper.py

--- vald_scraper.py
+++ vald_scraper.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 import requests
 
 BOT_TOKEN = "7931205039:AAGgCaBrEthLALnJXPWhT_V1_YfD80SvIIA"
 CHAT_ID = "7952868775"
-
 URL = "https://www.ticketmaster.fr/fr/manifestation/vald-billet/idmanif/618630"
 
 def send_telegram(msg):
@@ -13,11 +14,11 @@ def send_telegram(msg):
                   data={"chat_id": CHAT_ID, "text": msg})
 
 options = Options()
-options.add_argument("--headless")  # pour ne pas ouvrir la fenêtre
-driver = webdriver.Chrome(options=options)
-driver.get(URL)
-time.sleep(5)  # laisse la page charger
+options.add_argument("--headless")  # mode sans fenêtre
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
+driver.get(URL)
+time.sleep(5)  # laisse le JS charger
 html = driver.page_source
 
 if "Catégorie 3" in html and "Épuisé" not in html and "Sold Out" not in html:
